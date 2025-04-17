5bb2789890d650e9afe5d174415fe688b31b06c6
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-04-17 17:37:34 +0200

    Removed integration notification beans from controllers tests

--- src/test/java/com/sivalabs/ft/features/AbstractIT.java
+++ src/test/java/com/sivalabs/ft/features/AbstractIT.java
@@ -2,10 +2,13 @@ package com.sivalabs.ft.features;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import com.sivalabs.ft.features.integration.EventPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
@@ -13,7 +16,12 @@ import org.springframework.test.web.servlet.assertj.MockMvcTester;
 @AutoConfigureMockMvc
 @Import(DatabaseConfiguration.class)
 @Sql(scripts = {"/test-data.sql"})
+@TestPropertySource("classpath:application-test.properties")
 public abstract class AbstractIT {
+
+    @MockitoBean("eventPublisher")
+    EventPublisher eventPublisher;
+    
     @Autowired
     protected MockMvcTester mvc;
 }
